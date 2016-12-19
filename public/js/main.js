16c8f743efbbe21d1f2686716c3376c09a21741a
sadi <shifat2sadi@gmail.com>
2016-12-19 23:10:01 +0000

    Create post page added with Registration feature

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,69 @@
+ var config = {
+    apiKey: "AIzaSyBYSz3_wDygl1aC3-qYMNMKHJFhGMDh850",
+    authDomain: "self-help-hotline-acp.firebaseapp.com",
+    databaseURL: "https://self-help-hotline-acp.firebaseio.com",
+    storageBucket: "self-help-hotline-acp.appspot.com",
+    messagingSenderId: "746864609145"
+};
+firebase.initializeApp(config);
+
 $(document).ready(function(){
     
-    /*$(".search-form").submit(function(e){
+    var database = firebase.database();
+    var loggedInUsername = localStorage['username'];
+    
+	 
+    function writeUserData(username, email, password) {
+	  firebase.database().ref('users/' + username).set({
+	    username: username,
+	    password: password,
+	    email: email
+	  });
+	}
+	
+    
+    $(".register-form").submit(function(e){
         e.preventDefault();
-        var keyword = $("#keyword").val();
-        var category = $("#category").val();
-        location.href = "#/search?keyword=" + keyword + "&category=" + category;
-        return false;
         
-    })*/
+        console.log("Submitted"); 
+        var username = $("#username").val();
+        var email = $("#email").val();
+        var password = $("#password").val();
+        var confirm = $("#confirm-password").val();
+        
+        if(password == confirm){
+            
+            var searchUser = firebase.database().ref("users/" + username).limitToFirst(1);
+            searchUser.once("value", function(data) {
+            	var dataOutput = data.val();
+              	if(dataOutput){
+              		alert("This username is already in use");
+              	}else{
+              		writeUserData(username, email, password);
+              		console.log("You have been successfully registered!");
+              		if (!loggedInUsername) {
+                        localStorage['username'] = username;
+                    }
+                    console.log(localStorage['username']);
+              	}
+            }, function (error) {
+               console.log("Error: " + error.code);
+            });
+            
+            $(this)[0].reset();
+            
+        }else{
+            alert("Passwords do not match");
+        }
+       
+       return false; 
+    });
     
 })
+
+
+
+
+
+
 
